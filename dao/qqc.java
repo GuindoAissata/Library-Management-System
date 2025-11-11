71c2319627392d06df92cb2a79dc3a5adb21289d
Nono2905 <147551916+Nono2905@users.noreply.github.com>
2025-11-12 00:36:11 +0100

    Implement AdherentDAO with add and get methods

--- dao/qqc.java
+++ dao/qqc.java
@@ -1,1 +1,20 @@
+package sgeb.dao;
+
+
+import sgeb.model.Adherent;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AdherentDAO {
+    
+    private List<Adherent> listeAdherents = new ArrayList<>();
+
+    public void add(Adherent adherent) {
+        listeAdherents.add(adherent);
+    }
+
+    public List<Adherent> getlisteAdherents() {
+        return listeAdherents;
+    }
+}
 
