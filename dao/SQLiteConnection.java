38cf7f0e6a031210bcdc717401987f81077e7457
Nono2905 <147551916+Nono2905@users.noreply.github.com>
2025-11-13 10:19:24 +0100

    Refactor SQLiteConnection for new database and error handling
    Updated database URL and added error handling for connection.

--- dao/SQLiteConnection.java
+++ dao/SQLiteConnection.java
@@ -5,10 +5,23 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class SQLiteConnection {
-    private static final String URL = "jdbc:sqlite:./data/sgeb.db";
+    private static final String URL = "jdbc:sqlite:./data/Bibliotheque.db";
 
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL);
+        try{
+            Class.forName("org.sqlite.JDBC");
+            Connection conn = DriverManager.getConnection(URL);
+            System.out.println("Connexion SQLite établie avec succès !");
+            return conn;
+        }
+        catch(ClassNotFoundException e){
+            System.err.println("Erreur de chargement du driver : " + e.getMessage());
+            return null;
+        }
+        catch(SQLException e){
+            System.err.println("Erreur de connexion : " + e.getMessage());
+            return null;
+        }
     }
 }
 
