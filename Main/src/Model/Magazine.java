9709207650d8d8997e07bd909150ef5d1ba3856c
Nono2905 <147551916+Nono2905@users.noreply.github.com>
2025-11-13 18:15:45 +0100

    Update Magazine.java
    - Pas d'auteur pour un magazine (récupérer editeur depuis classe mere document)
    - Périodicité ("Mensuel", "hebdomadaire", etc) donc g crée un champ à part pck c différent de ISSN
    - Numéro c'est genre le 1er, 5e, 40e magazine depuis la création du magazine

--- Main/src/Model/Magazine.java
+++ Main/src/Model/Magazine.java
@@ -7,36 +7,39 @@ import java.util.*;
  */
 public class Magazine extends Document {
     private String ISSN;
-    private Double numero;
+    private int numero;
+    private String periodicite; // dire si c'est hebdomadaire, mensuel, trimestriel
 
     /**
      * Default constructor
      */
-    public Magazine(int id, String titre, String auteur, double numero, String ISSN) {
-        super(id, titre, auteur);
+    public Magazine(int id, String titre, String editeur, int numero, String periodicite, String ISSN) {
+        super(id, titre, editeur);
         this.numero = numero; 
         this.ISSN = ISSN;
+        this.periodicite = periodicite;
     }
 
     /**
      * 
      */
      //////////////getter////////
-    public String getNumero_peridicite(){return  ISSN;}
-    public Double getNumero(){return numero;}
+    public String getISSN(){return  ISSN;}
+    public int getNumero(){return numero;}
+    public String getPeriodicite(){return periodicite;}
     
     /////////   Setter////////
-    public void setNumero_periodicite(String n ){  ISSN = n;}
-    public void setNumero(Double n){ numero = n;}
+    public void setISSN(String n ){  ISSN = n;}
+    public void setNumero(int n){ numero = n;}
+    public void setPeriodicite(String p){periodicite = p;}
     
     ////////////genre //////////
     @Override
     public String getType() {
-
         return "Magazine";
     }
     public String toString(){
-        return super.toString() + " ; Genre : " + getType() + " ; Numero : " + numero + " ; Numero_periodicité : " +numero_periodicite;
+        return super.toString() + " ; Genre : " + getType() + " ; Numero : " + numero + " ; periodicité : " +periodicite + " ; ISSN : " + ISSN;
     }
     
 
