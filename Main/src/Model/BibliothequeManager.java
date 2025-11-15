1a68c2ae01035a9a56a174ba24eae887adfe7c1a
Aissata GUINDO <153300440+GuindoAissata@users.noreply.github.com>
2025-11-15 12:53:14 +0100

    Add Emprunt and Retour methods
    Add borrowing and returning methods in BibliothequeManager.
    Ajout des méthodes "Emprunt" et "Retour"

--- Main/src/Model/BibliothequeManager.java
+++ Main/src/Model/BibliothequeManager.java
@@ -28,19 +28,6 @@ public class BibliothequeManager {
      */
     public Set<Adherent> List_Adherent = new HashSet<>();
 
-    /**
-     * @param d
-     */
-    public void Emprunt(Document d) {
-        // TODO implement here
-    }
-
-    /**
-     * @param d
-     */
-    public void Retour(Document d) {
-        // TODO implement here
-    }
 
     ///////Methode : Ajouter de nouveaux documents////// 
     public Boolean AddDocument(Document doc){
@@ -51,8 +38,7 @@ public class BibliothequeManager {
     ///////Methode : Suppression d'un document if not borrowed //////
     public Boolean DeleteDocument(Document doc){
         if(doc.getEstDisponible()==true){// If the document has not been borrowed
-            List_Document.remove(doc);
-            return true;
+          return  List_Document.remove(doc);
         } return false;
         
     }
@@ -75,17 +61,37 @@ public class BibliothequeManager {
     }
 
 
-    /////////////Enregistrer unn Emprunt//////////////
-    public boolean Enregistrer_Emprunt (Document document,Adherent adherent) throws ArgumentException{
+   /////////////Enregistrer unn Emprunt//////////////
+    public LocalDate Enregistrer_Emprunt (Document document,Adherent adherent) throws ArgumentException{
         if (!document.getEstDisponible()) {
             throw new ArgumentException("Le document n'ets pas disponible"); 
         }
         if (adherent.getNb_Emprunt_Encours()>=5){
             throw new ArgumentException("Le nombre d'emprunts autorisé est atteint");
         }
-        if(adherent.getPenalite()>0){
+        if(adherent.getPenalite()>=10){// s'il a des  penalités majeures (j'ai choisi 10) 
             throw new ArgumentException("L'adherent a des pénalités à régler");
         }
+        Emprunt emprunt = new Emprunt(adherent, document);
+        List_Emprunt.add(emprunt); // Ajout dans la liste des emprunts de la Bibliothèque
+        adherent.AddEmpruntAdherent(emprunt); //Ajout dans la liste personnelle des emprunts de l'adhérent
+        document.AddEmpruntDoc(emprunt); //Ajuot dans la liste des emprunts concernés par le document
+        document.setEstDisponible(false);// rendre indisponible le document 
+        return emprunt.getDate_RetourPrevue();
+    }
+
+    /////Enregistrer un retour//////////////////////
+    public boolean Enregistrer_Retour(Emprunt emprunt){
+        emprunt.setDateRetourRelle(LocalDate.now());
+        if(emprunt.getDate_RetourReelle().isAfter(emprunt.getDate_RetourPrevue())){
+            Long retard = ChronoUnit.DAYS.between(emprunt.getDate_RetourReelle(),emprunt.getDate_RetourReelle()) ;
+            Double penalite = 0.50 * retard;
+            emprunt.setPenalite(penalite);//on met à jour la pénalité de l'emprunt
+            emprunt.adherent.setPenalite(penalite);//Ajouter la pénalité à celle existante de l'adherent
+        } 
+            emprunt.adherent.setNb_Emprunt_Encours();// on enlève 1 à son nbre d'emprunt en cours
+            emprunt.document.setEstDisponible(true);// On rend disponible le livre
+            emprunt.setStatut_Emprunt(false);// on met à false pour dire que l'emprunt n'est plus en cours 
         return true;
     }
 
