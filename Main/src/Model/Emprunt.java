e332a5f8ee641073ce08c7c6054e904745e458bb
Aissata GUINDO <153300440+GuindoAissata@users.noreply.github.com>
2025-11-15 12:28:26 +0100

    Refactor Emprunt class variable names to camelCase

--- Main/src/Model/Emprunt.java
+++ Main/src/Model/Emprunt.java
@@ -32,16 +32,16 @@ public class Emprunt {
     /*
      * 
      */
-    private Boolean Statut_Emprunt; // ajouter pour savoir si emprunt en cours ou non 
+    private Boolean statut_Emprunt; // ajouter pour savoir si emprunt en cours ou non 
     /**
      * 
      */
-    public Document Document;
+    public Document document;
 
     /**
      * 
      */
-    public Adherent Adherent;
+    public Adherent adherent;
 
     /**
      * 
@@ -52,12 +52,12 @@ public class Emprunt {
      * Default constructor
      */
     // Pour créer un emprunt j'ai besoin de renseigner l'adherent qui prête et le document concerné par le prêt
-    public Emprunt(Adherent Adherent, Document Document) {
+    public Emprunt(Adherent adherent, Document document) {
         this.Date_Emprunt = LocalDate.now();// Annee/mois/jour au moment du prêt
         this.Date_RetourPrevue = Date_Emprunt.plusMonths(1);// retour 1 mois apèrs l'emprunt
-        this.Adherent= Adherent;
-        this.Document = Document;
-        this.Statut_Emprunt = true;
+        this.adherent= adherent;
+        this.document = document;
+        this.statut_Emprunt = true;
         // L'id sera automatiquement renseigné à la création d'un emprunt
         // l'id sera l'annee+mois+jour+minute+seconde du prêt 
         LocalDateTime time = LocalDateTime.now();
@@ -67,16 +67,15 @@ public class Emprunt {
 
 ///////////////getter////////////////
     public Long getID_Emprunt(){  return ID_Emprunt;}
-    public Document getDocument(){ return Document;}
-    public Adherent getAdherent(){ return Adherent;}
     public LocalDate getDate_Emprunt(){ return Date_Emprunt;}
     public LocalDate getDate_RetourPrevue(){return Date_RetourPrevue; }
     public LocalDate getDate_RetourReelle(){return Date_RetourReelle;}
-    public Boolean getStatut_Emprunt(){return Statut_Emprunt;}
+    public Boolean getStatut_Emprunt(){return statut_Emprunt;}
 
     ////////////////setter////////////////
     public void setID_Emprunt( Long id){ ID_Emprunt = id;}
     public void setDateRetourPrevu( LocalDate d){ Date_RetourPrevue = d;}
     public void setDateRetourRelle(LocalDate d){ Date_RetourReelle = d;}
+    public void setStatut_Emprunt(Boolean s){ statut_Emprunt = s;}
 
 }
