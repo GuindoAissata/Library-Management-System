f1d7794b37ad4dd4c05651b6d42c43d283693cd0
Aissata <aissatag203@gmail.com>
2026-01-06 15:29:27 +0100

    :wq

--- Main/run.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d "%~dp0"
-
-java --module-path ".\javafx-sdk-21.0.2\lib" --add-modules javafx.controls,javafx.fxml -cp ".;GestionBiblio.jar;lib\sqlite-jdbc-3.50.3.0.jar" App
-
-pause
--- Main/src/Model/ArgumentException.java
+++
@@ -1,8 +0,0 @@
-package Model;
-
-public class ArgumentException extends Exception {
-    private String message;
-    public ArgumentException(String m){ message = m ; }
-    public String getMessage(){return message;}
-    
-}
